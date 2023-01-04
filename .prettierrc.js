1919fa6dcb5bafc2ddac0727a553fbad1643d2e9
Superduper-India <sunyoung1542@gmail.com>
2023-01-04 15:16:26 +0900

    모듈 추가 설치 및 개발환경 설정변경 close #1 리액트 리덕스, sort imports 프리티어 플러그인 모듈을 설치했습니다. 그 밖에 린트 설정을 변경했습니다.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,11 @@
 module.exports = {
-  semi: true,
-  singleQuote: true,
-  printWidth: 100,
-  tabWidth: 2,
-  trailingComma: 'es5',
-  arrowParens: 'always',
+    semi: true,
+    singleQuote: true,
+    printWidth: 100,
+    tabWidth: 2,
+    trailingComma: 'es5',
+    arrowParens: 'always',
+    importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
 };
