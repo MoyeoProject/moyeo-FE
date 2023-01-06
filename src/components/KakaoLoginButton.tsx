6bd72c335c51f2042be98957e51b6e0b2b5bc962
thdud <thgml1444@naver.com>
2023-01-07 01:21:13 +0900

    kakaoLogin 구현중

--- src/components/KakaoLoginButton.tsx
+++ src/components/KakaoLoginButton.tsx
@@ -1,16 +1,32 @@
-const KAKAO_AUTH_URL =
-  'https://kauth.kakao.com/oauth/authorize?client_id=ced49bfdb65f5f152e2e43f12e88bd86&redirect_uri=https://sparta-hippo.shop/api/user/kakao/callback&response_type=code';
+import axios from 'axios';
+import { useEffect } from 'react';
+
+export const KAKAO_AUTH_URL =
+  'https://kauth.kakao.com/oauth/authorize?client_id=ced49bfdb65f5f152e2e43f12e88bd86&redirect_uri=http://localhost:3000/api/users/kakao/callback&response_type=code';
 
 const KakaoLoginButton = () => {
-  return (
-    <p
-      onClick={() => {
-        console.log(KAKAO_AUTH_URL);
-      }}
-    >
-      카카오로그인
-    </p>
-  );
+  const code = new URL(window.location.href).searchParams.get('code');
+  console.log('9', code);
+
+  useEffect(() => {
+    try {
+      const getToken = async () => {
+        const data = await axios.get(
+          `https://sparta-hippo.shop/api/users/kakao/callback?code=${code}`
+        );
+        localStorage.setItem('id', data.data.data.jwtToken);
+        console.log(data);
+        return data;
+      };
+      getToken().then(() => window.location.assign('/home'));
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+  return <>카카오 리다이렉트 페이지에서 data 확인하고 다음 처리하기</>;
 };
 
 export default KakaoLoginButton;
+
+// 캐싱되는 이유 // 리액트쿼리 캐싱 무효화.
+// 304 에러
---
+++ src/pages/DetailPage.tsx
@@ -0,0 +1,49 @@
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+const DetailPage = () => {
+  const navigate = useNavigate();
+  const [categories, setCategoreis] = useState('intro');
+  return (
+    <>
+      <div>
+        <button
+          onClick={() => {
+            navigate('/main');
+          }}
+        >
+          뒤로가기
+        </button>
+        <span>모임이름</span>
+        <button>알림아이콘</button>
+        <button>모임공유</button>
+        <button>수정아이콘</button>
+      </div>
+
+      <div>
+        <button type="button" onClick={() => setCategoreis('intro')}>
+          소개
+        </button>
+        <button type="button" onClick={() => setCategoreis('comment')}>
+          댓글
+        </button>
+      </div>
+
+      {categories === 'intro' ? (
+        <>
+          <h2>소개</h2>
+          <div>DetailIntro</div>
+        </>
+      ) : (
+        <>
+          <h2>댓글</h2>
+          <div>DetailComment</div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default DetailPage;
