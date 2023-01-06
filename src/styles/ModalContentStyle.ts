91b5caee661c2a9c937ef59eb0a7b063260c8642
Superduper-India <sunyoung1542@gmail.com>
2023-01-06 22:16:08 +0900

    전역스타일과 모달스타일을 적용했습니다.

---
+++ src/styles/ModalContentStyle.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ModalContentStyle = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  box-shadow: rgba(100, 100, 111, 0.3) 0px 7px 29px 0px;
+  background-color: white;
+  border: 2px solid rgb(240, 240, 240);
+  border-radius: 12px;
+  position: absolute;
+  width: 250px;
+  top: 70px;
+  left: calc(50% - 125px);
+  bottom: 70px;
+`;
