2dc08a5539d514bbaad4fe546180eebcd203ac0e
thdud <thgml1444@naver.com>
2023-01-07 09:47:15 +0900

    #14 카카오 로그인 구현중

--- src/components/KakaoLoginButton.tsx
+++ src/components/KakaoLoginButton.tsx
@@ -4,29 +4,37 @@ import { useEffect } from 'react';
 export const KAKAO_AUTH_URL =
   'https://kauth.kakao.com/oauth/authorize?client_id=ced49bfdb65f5f152e2e43f12e88bd86&redirect_uri=http://localhost:3000/api/users/kakao/callback&response_type=code';
 
-const KakaoLoginButton = () => {
-  const code = new URL(window.location.href).searchParams.get('code');
-  console.log('9', code);
+const setAccessToken = (accessToken: any) => {
+  localStorage.setItem('is_login', accessToken);
+};
 
-  useEffect(() => {
-    try {
-      const getToken = async () => {
-        const data = await axios.get(
-          `https://sparta-hippo.shop/api/users/kakao/callback?code=${code}`
-        );
-        localStorage.setItem('id', data.data.data.jwtToken);
-        console.log(data);
-        return data;
-      };
-      getToken().then(() => window.location.assign('/home'));
-    } catch (err) {
-      console.log(err);
-    }
-  }, []);
-  return <>카카오 리다이렉트 페이지에서 data 확인하고 다음 처리하기</>;
+const kakaoLogin = (code: string | null) => {
+  console.log(code);
+  axios
+    .get(`https://sparta-hippo.shop/api/users/kakao/callback?code=${code}`)
+    .then((res) => {
+      console.log(res);
+      // console.log(res.headers.authorization); // 토큰이 넘어올 것임
+      // const accessToken = res.headers.authorization;
+      // setAccessToken(accessToken);
+
+      // window.location.href = '/main'; // 토큰 받았았고 로그인됐으니 화면 전환시켜줌(메인으로)
+    })
+    .catch((err: any) => {
+      console.log('소셜로그인 에러', err);
+      // window.alert('로그인에 실패하였습니다.');
+      // window.location.href = '/'; // 로그인 실패하면 로그인화면으로 돌려보냄
+    });
+  //   };
+};
+
+const KakaoLoginButton = () => {
+  // 인가코드
+  console.log('호출 직전!!!');
+  const code: string | null = new URL(window.location.href).searchParams.get('code');
+  console.log(code);
+  kakaoLogin(code);
+  return <div>카카오 로그인 처리중...</div>;
 };
 
 export default KakaoLoginButton;
-
-// 캐싱되는 이유 // 리액트쿼리 캐싱 무효화.
-// 304 에러
