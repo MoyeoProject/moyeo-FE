cb0a83296b4b2a74da6ce082e6307bf5a9c7448e
thdud <thgml1444@naver.com>
2023-01-12 12:13:11 +0900

    #40 detailpage 하단에 모임 참석 버튼, master/member에 따라 구분, 비참여는 버튼 접근안되게 설정

--- src/components/common/DetailButton.tsx
+++ src/components/common/DetailButton.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
+import { meetAttendExitApi } from '../../services/api';
 import { useAppSelector } from '../../store';
 
-const DetailButton = () => {
-  const isMaster = useAppSelector((state) => state.auth.isMaster);
+const DetailButton = ({ data }: any) => {
+  const { id } = useParams();
+  const QueryClient = useQueryClient();
 
+  const handleClickAttnedExit = () => {
+    console.log('참석/취소버튼 api같음');
+  };
   const meetingLink = () => {
     // 모임 입장 링크 입력
   };
   const meetingEntrance = () => {
     // 멤버 모임입장
   };
+
+  const useMeetAttendExit = () => {
+    return useMutation(meetAttendExitApi, {
+      onSuccess: () => {
+        QueryClient.invalidateQueries();
+      },
+      onError: (err: any) => {
+        return alert(err.response.data.statusMsg);
+      },
+    });
+  };
+
+  const { mutate: meetAttendExit } = useMeetAttendExit();
+  const handleClickAttned = (id: any) => {
+    meetAttendExit(id);
+    // ClickAttnedExit(id);
+    // DetailNavBar랑 겹침 공통으로 쓸 수 있게 hook으로 빼보기
+  };
+
   return (
-    <>
-      {isMaster ? (
+    <div>
+      {data?.master ? (
         <button onClick={meetingLink}>입장 링크를 입력해주세요</button>
+      ) : data?.attend ? (
+        <button onClick={meetingEntrance}>모임 입장</button>
       ) : (
-        <button onClick={meetingEntrance}>모임 입장 / 모임이 시작되지 않았습니다</button>
+        <button
+          onClick={() => {
+            handleClickAttned(id);
+          }}
+        >
+          모임에 참석한 후 입장 가능합니다
+        </button>
       )}
-    </>
+    </div>
   );
 };
 
