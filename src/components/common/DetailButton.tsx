66ea94766cc6b667f043796b9c0f48e9e1ff5878
thdud <thgml1444@naver.com>
2023-01-13 01:15:55 +0900

    #53 [S-2] 상세페이지 모임 입장버튼 생성 (master, member 각각 구현)

--- src/components/common/DetailButton.tsx
+++ src/components/common/DetailButton.tsx
@@ -1,22 +1,88 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { meetingLinkInpitApi } from '../../services/api';
+
 const DetailButton = ({ data }: any) => {
-  const meetingLink = () => {
-    // 모임 입장 링크 입력
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [link, setLink] = useState('');
+  const [linkInput, setLinkInput] = useState(false);
+  const QueryClient = useQueryClient();
+
+  console.log(data);
+
+  const useMeetingLinkInput = () => {
+    return useMutation(meetingLinkInpitApi, {
+      onSuccess: () => {
+        alert('모임 링크가 개설되었습니다');
+        QueryClient.invalidateQueries(['link', id]);
+      },
+      onError: (data: any) => {
+        alert(data?.response.data.statusMsg);
+      },
+    });
   };
-  const meetingEntrance = () => {
-    // 멤버 모임입장, 링크로 이동
-    alert(`${data.platform}으로 입장합니다`);
+
+  const { mutate: meetingLinkInput } = useMeetingLinkInput();
+  const meetingLinkInputBtn = () => {
+    if (link === '') {
+      alert('링크를 입력해주세요');
+      return;
+    }
+    meetingLinkInput({ link, platform: data.platform, id });
+  };
+
+  const meetingEntranceBtn = () => {
+    if (data.link) {
+      alert(`${data.platform}으로 입장합니다`);
+      window.location.href = `/${data.link}`;
+    } else {
+      alert('모임 시작 30분 전부터 입장 가능합니다');
+    }
   };
 
   return (
-    <div>
-      {data?.master ? (
-        <button onClick={meetingLink}>입장 링크를 입력해주세요</button>
-      ) : data?.attend ? (
-        <button onClick={meetingEntrance}>모임 입장</button>
-      ) : (
-        <div>모임에 참석한 후 입장 가능합니다</div>
-      )}
-    </div>
+    <>
+      <div>
+        {data?.master ? (
+          linkInput ? (
+            <div>
+              <input
+                type="text"
+                placeholder="입장 링크 입력"
+                onChange={(e) => setLink(e.target.value)}
+              />
+              <button onClick={meetingLinkInputBtn}>입력</button>
+            </div>
+          ) : data.link ? (
+            <>
+              <button onClick={meetingEntranceBtn}>모임 입장</button>
+              <button
+                onClick={() => {
+                  setLinkInput(true);
+                }}
+              >
+                입장 링크 수정
+              </button>
+            </>
+          ) : (
+            <button
+              onClick={() => {
+                setLinkInput(true);
+              }}
+            >
+              입장 링크를 입력해주세요
+            </button>
+          )
+        ) : data?.attend ? (
+          <button onClick={meetingEntranceBtn}>모임 입장</button>
+        ) : (
+          <div>모임에 참석한 후 입장 가능합니다</div>
+        )}
+      </div>
+    </>
   );
 };
 
