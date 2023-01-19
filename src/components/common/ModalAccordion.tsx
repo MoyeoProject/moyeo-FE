b7225bc5ad0423261cb819eb5299b1f6f88b072e
Superduper-India <sunyoung1542@gmail.com>
2023-01-19 16:48:01 +0900

    그 외 스타일 컴포넌트 이름 변경 ModalFormStyle에서 ModalStyle로 변경하고, 이엔 모달에 해당하는 스타일만 포함 나머지 폼에 관련된 스타일은 FormStyle에 들어있습니다.
    이전 커밋에서 누락된 탑네브바 로고이미지 추가

--- src/components/common/ModalAccordion.tsx
+++ src/components/common/ModalAccordion.tsx
@@ -3,7 +3,7 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { Control, Controller, FieldValues } from 'react-hook-form';
 
-import { ModalFormWrap, Overlay } from '../../styles/ModalFormStyle';
+import { ModalWrap, Overlay } from '../../styles/ModalStyle';
 import { calcStartDate } from '../../utils/utils';
 
 type ModalAccordionProps = {
@@ -25,7 +25,7 @@ export default function ModalAccordion({
 
   return (
     <Overlay>
-      <ModalFormWrap>
+      <ModalWrap>
         <p>{title}</p>
         {name === 'startDate' ? (
           <>
@@ -81,7 +81,7 @@ export default function ModalAccordion({
             </button>
           </>
         )}
-      </ModalFormWrap>
+      </ModalWrap>
     </Overlay>
   );
 }
