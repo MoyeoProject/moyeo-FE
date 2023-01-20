c2cad31656a061b4d497dd9fb13779f7650c1586
thdud <thgml1444@naver.com>
2023-01-20 22:08:44 +0900

    모임 인원 다 찼을 경우, 버튼에 문구 띄워주기 기능 추가. 마스터 모임 입장 / 링크 수정 버튼 마진 간격 조절

--- src/components/common/DetailButton.tsx
+++ src/components/common/DetailButton.tsx
@@ -1,41 +1,36 @@
 import { useQueryClient } from '@tanstack/react-query';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { useParams } from 'react-router-dom';
 
-import {
-  ButtonBasic,
-  ButtonDisabled,
-  ButtonEdit,
-  MasterButton,
-} from '../../styles/DetailButtonStyle';
+import { loadItem } from '../../services/storage';
+import { ButtonBasic, MasterButton } from '../../styles/DetailButtonStyle';
 import DetailMeetLinkButton from '../DetailMeetLinkButton';
 
-const DetailButton = ({ data }: any) => {
+const DetailButton = ({ data, member }: any) => {
   const [showModal, setShowModal] = useState(false);
-
-  const meetingLinkedit = () => {
-    // 입장링크 수정
-  };
-
   const meetingEntranceBtn = () => {
     if (data.link) {
-      alert(`${data.platform}으로 입장합니다`);
-      window.open(`http://${data?.link}`);
+      window.open(`${data?.link}`);
     }
     // else {
     //  알람기능 되면 test
     //  alert('모임 시작 30분 전부터 입장 가능합니다');
     // }
   };
+
   return (
     <>
       {data?.master ? (
         <>
           {data?.link !== '' ? (
             <MasterButton>
-              <ButtonBasic onClick={meetingEntranceBtn}>모임입장</ButtonBasic>
-              <ButtonEdit onClick={() => setShowModal(true)}>입장 링크 수정</ButtonEdit>
+              <ButtonBasic activeBtn={true} onClick={meetingEntranceBtn}>
+                모임입장
+              </ButtonBasic>
+              <ButtonBasic activeBtn={false} onClick={() => setShowModal(true)}>
+                입장 링크 수정
+              </ButtonBasic>
               {showModal &&
                 createPortal(
                   <DetailMeetLinkButton
@@ -48,7 +43,9 @@ const DetailButton = ({ data }: any) => {
             </MasterButton>
           ) : (
             <>
-              <ButtonBasic onClick={() => setShowModal(true)}>입장 링크를 입력해주세요</ButtonBasic>
+              <ButtonBasic activeBtn={false} onClick={() => setShowModal(true)}>
+                입장 링크를 입력해주세요
+              </ButtonBasic>
               {showModal &&
                 createPortal(
                   <DetailMeetLinkButton
@@ -62,9 +59,13 @@ const DetailButton = ({ data }: any) => {
           )}
         </>
       ) : data?.attend ? (
-        <ButtonBasic onClick={meetingEntranceBtn}>모임 입장</ButtonBasic>
+        <ButtonBasic onClick={meetingEntranceBtn} activeBtn={true}>
+          모임 입장
+        </ButtonBasic>
+      ) : member.length === data.maxNum ? (
+        <ButtonBasic activeBtn={false}>정원이 다 찼습니다</ButtonBasic>
       ) : (
-        <ButtonDisabled>모임에 참석한 후 입장 가능합니다</ButtonDisabled>
+        <ButtonBasic activeBtn={false}>모임에 참석한 후 입장 가능합니다</ButtonBasic>
       )}
     </>
   );
