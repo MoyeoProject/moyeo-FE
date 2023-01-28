c159c65600fc9f16a53197c65f04009d5847530e
thdud <thgml1444@naver.com>
2023-01-28 16:53:07 +0900

    임시 페이지에 버튼 생성하여 세부 페이지 구현

---
+++ src/components/InfoPrivacy.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { InfoBox } from '../styles/InfoBoxStyle';
+import { InfoNav } from '../pages/FollowPage';
+
+export const InfoPrivacy = () => {
+  return (
+    <InfoBox>
+      <InfoNav children={'개인정보'} />
+    </InfoBox>
+  );
+};
